5264fadeb5f43a38ba36ae935367b2f865db0727
Joon <jtj308@naver.com>
2020-10-21 00:50:05 +0900

    Updates

---
+++ precache-manifest.17805cc918f8ad05d626870996c98b1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7342f182102461caec3479e5975bae",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "27e4d4c991f0f74d8aa2",
+    "url": "/monster-rolodex/static/css/main.8b2f6df1.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monster-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monster-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e4d4c991f0f74d8aa2",
+    "url": "/monster-rolodex/static/js/main.459fa21a.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
