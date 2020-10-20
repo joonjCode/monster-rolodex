28063191e1eb19f747630f1929386143c837f43d
Joon <jtj308@naver.com>
2020-10-21 00:28:36 +0900

    arrow functions done

--- src/App.js
+++ src/App.js
@@ -2,33 +2,38 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import { CardList } from './components/card-list/card-list.component';
 import './App.css';
-
+import './components/search-box/search-box.component';
+import { SearchBox } from './components/search-box/search-box.component';
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
       searchField: '',
-    }
-  };
+    };
+
+  }
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }))
 
   }
-
+  handleChange= (e)=> {
+    this.setState({ searchField: e.target.value })
+  }
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonster = monsters.filter(
       monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
-    )
+    );
     return (
       <div className="App">
-        <input
-          type='search'
+        <SearchBox
           placeholder='search monster'
-          onChange={e => this.setState({ searchField: e.target.value })} />
+          handleChange={this.handleChange}
+        />
+
         <CardList monsters={filteredMonster} />
       </div>
     );
